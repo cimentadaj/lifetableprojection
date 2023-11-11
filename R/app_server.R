69f3cd955769bf5df88ed603489beada3c3c88de
Jorge Cimentada <cimentadaj@gmail.com>
2023-11-11 14:48:43 +0100

    Adds data parser alerts

--- R/app_server.R
+++ R/app_server.R
@@ -4,6 +4,27 @@
 #' managing data processing, UI rendering, and routing.
 #'
 #' @param input,output,session Internal parameters for `{shiny}`.
+#' @importFrom shiny renderUI req wellPanel HTML
+#' @importFrom tools toTitlese
+#' @importFrom utils read.csv
+#' @importFrom ODAP.backend check_data
 #' @export
 app_server <- function(input, output, session) {
+    output$validation_results <- renderUI({
+        req(input$file1)
+        # Assuming 'data_in' is the processed data from the uploaded file
+        data_in <- read.csv(input$file1$datapath)
+        check_results <- check_data(data_in)
+
+        if (all(check_results$pass == "Pass")) {
+            wellPanel(class = "success", "\u2705 Everything is great, all checks passed!")
+        } else {
+            failed_checks <- check_results[check_results$pass != "Pass", ]
+            wellPanel(
+                class = "danger",
+                HTML("Check Failures:<br>"),
+                HTML(paste("\u274C", toTitleCase(failed_checks$check), collapse = "<br>"))
+            )
+        }
+    })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -49,28 +49,91 @@ input_page <- function() {
 #'
 #' @param request Internal parameter for `{shiny}`.
 #' @return A shiny semantic UI for the application.
-#' @importFrom shiny div
+#' @importFrom shiny div HTML h1 p uiOutput
 #' @importFrom shinyjs useShinyjs
-#' @importFrom shiny.semantic main_panel action_button selectInput
+#' @importFrom shiny.semantic main_panel action_button selectInput file_input
 #' @importFrom untheme fluidUnTheme
 #' @noRd
 app_ui <- function(request) {
   fluidUnTheme(
     useShinyjs(),
     main_panel(
+      tags$head(
+        tags$style(HTML("
+            .centered {
+                position: absolute;
+                top: 30%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+            .semi-centered {
+                position: absolute;
+                left: 50%;
+                transform: translateX(-50%);
+            }
+            .info-box {
+                border: 1px solid #ddd;
+                border-radius: 5px;
+                padding: 20px;
+                margin-bottom: 20px;
+                text-align: center;
+                color: #555;
+            }
+            .info-box h1 {
+                color: #337ab7;
+            }
+            .info-box p {
+                font-size: 16px;
+            }
+            .validation-results {
+                text-align: left;
+                font-size: 15px; /* Adjusted font size */
+                color: #555;
+                font-family: 'Arial', sans-serif; /* Changed font family */
+                font-weight: bold; /* Making text bold */
+            }
+            .well {
+                background-color: #f8d7da; /* Light red for error */
+                border-color: #f5c6cb;
+                border-radius: 5px;
+                padding: 15px;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Added shadow */
+            }
+            .well.success {
+                background-color: #d4edda; /* Light green for success */
+                border-color: #c3e6cb;
+            }
+            .emoji {
+                vertical-align: middle;
+                font-size: 40px; /* Adjusted emoji size */
+            }
+        "))
+      ),
       div(
-        id = "step1",
-        class = "ui raised very padded text container segment",
+        class = "centered",
+        tags$div(
+          style = "text-align: left;",
+          class = "info-box",
+          h1("Welcome to the Data Uploader! \U0001F680"),
+          p("Upload your CSV file with the button below. Make sure it follows the format:"),
+          p("\U0001F4C4 Headers on the first row."),
+          p("\U0001F522 Numeric and text fields properly arranged."),
+          p("If there are any issues with your file, we'll let you know!")
+        ),
         div(
-          class = "ui form",
-          input_page(),
-          action_button("forward_step2", "Next", class = "ui blue button")
-        )
+          class = "semi-centered",
+          file_input("file1", ""),
+          div(
+            class = "validation-results",
+            uiOutput("validation_results")
+          )
+        ),
       ),
       width = NULL
     )
   )
 }
+
 
 #' Add external Resources to the Application
 #'
