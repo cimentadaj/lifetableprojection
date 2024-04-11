9fe839018186e841bb9c6a85c383b18c3a50d8f7
Jorge Cimentada <cimentadaj@gmail.com>
2024-04-11 14:47:54 +0200

    Moves lifetable into separate file

---
+++ R/lifetable_calculation.R
@@ -0,0 +1,35 @@
+#' Calculate Life Table
+#'
+#' Performs life table calculations based on user input and the uploaded data.
+#'
+#' @param data_in Data frame containing mortality data.
+#' @param input List of parameters from Shiny input.
+#' @return List containing life table calculation results.
+#' @importFrom ODAPbackend lt_flexible
+#' @export
+calculateLifeTable <- function(data_in, input) {
+  req(input$calculate_lt)
+  input_extrapfrom <- as.numeric(input$input_extrapFrom)
+
+  begin_age <- which(data_in$Age == input$slider_ages_to_use[1])
+  end_age <- which(data_in$Age == input$slider_ages_to_use[2])
+  ages_to_use <- data_in$Age[begin_age:end_age]
+
+  library(ggplot2)
+
+  lt_res <- lt_flexible(
+    data_in = data_in,
+    OAnew = as.numeric(input$input_oanew),
+    age_out = input$input_age_out,
+    extrapFrom = input_extrapfrom,
+    extrapFit = ages_to_use,
+    extrapLaw = input$input_extrapLaw,
+    radix = as.numeric(input$input_radix),
+    SRB = as.numeric(input$input_srb),
+    a0rule = input$input_a0rule,
+    axmethod = input$input_axmethod,
+    Sex = input$input_sex
+  )
+
+  list(lt = lt_res, extrapfrom = input_extrapfrom)
+}
